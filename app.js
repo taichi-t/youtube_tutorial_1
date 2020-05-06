076a01f7fd917545d60dc2a363291b33d016697c
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-06 01:43:45 -0700

    1:02

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("your code goes here...");
+function add(n1, n2, showResult, Phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(Phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("your code goes here...");
+function add(n1: number, n2: number, showResult: boolean, Phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(Phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
