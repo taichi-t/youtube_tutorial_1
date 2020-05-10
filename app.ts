d80777d20cfb07c92cba6f39756eb81f11e4596d
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-10 02:16:33 -0700

    2:21:55

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ if (typeof userInput === "string") {
   userName = userInput;
 }
 
-function generateError(message: string, code: number) {
+function generateError(message: string, code: number): never {
   throw { message: message, errorCode: code };
 }
 
